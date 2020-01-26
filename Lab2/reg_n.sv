334f797c3c4140995a63fed8a1e3bf9dbdfacc8f
Naveed Ashfaq <naveedashfaq914@gmail.com>
2020-01-25 22:08:52 -0500

    Added n bit registers and booth encoder

---
+++ Lab2/reg_n.sv
@@ -0,0 +1,13 @@
+module reg_n #(parameter NUM_BITS) (
+	input i_clk,
+	input i_en,
+	input [NUM_BITS-1:0] i_data,
+	
+	output [NUM_BITS-1:0] o_data
+);
+
+	always_ff @(posedge i_clk) begin
+		if (i_en) o_data <= i_data;
+	end
+
+endmodule
